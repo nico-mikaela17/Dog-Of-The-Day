39b88cec5e240dd0c404bd8ac2edd1860657a2c2
Nicole Linnarz <mikaelafs17@gmail.com>
2023-12-05 21:04:29 -0700

    added api doc

---
+++ README.md
@@ -0,0 +1,12 @@
+# API Documentation
+
+## /api/dogs
+
+GET method - show fav dogs
+
+## /api/dog
+
+POST method - add dog to the array (make it fav) + comment  
+PUT method - Update info on a dog (edit user comment)  
+DELETE method - take dog + comment off favs
+
--- server.js
+++ server.js
@@ -8,9 +8,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
  
 app.use(express.static("client"));
 
+//Create a saved dog (card?) - POST
+// Update - PUT
+// Read - GET
+// Delete - DELETE
+
 // LISTEN
 app.listen(port, () => {
   console.log(`Dog app listening on port ${port}`);
 });
-
-
